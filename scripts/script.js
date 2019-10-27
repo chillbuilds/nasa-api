8c11d9c8cfaaae52ab7389b196c94f35774044f9
heliumface770 <heliumface770@gmail.com>
2019-10-26 18:40:02 -0600

    rover image display

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,45 @@
-var responseVar = "";
+var camChoice = "";
+var resp = "";
 var APIKey = "fzF4NiozSfJsw3dQiAaHoExxhSDBC9BqMwEzu8AP";
-var queryURL = "https://api.nasa.gov/planetary/apod?api_key=" + APIKey;
+var whichKey = 0;
+var queryURL = "";
+var camShort = ["FHAZ", "NAVCAM", "MAST", "RHAZ"]
+// ^ ^ ^ response.photos[1].rover.cameras[1].name
+var camLong = ["Front Hazard Avoidance Camera", "Navigation Camera", "Mast Camera", "Rear Hazard Avoidance Camera"]
 
-$.ajax({
+
+function ajaxCall(){
+    var year = $("#year").val();
+    var month = $("#month").val();
+    var day = $("#day").val();
+    queryURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="+year+"-"+month+"-"+day+"&api_key=" + APIKey;
+    $.ajax({
     url: queryURL,
     method: "GET"
   })
-    // We store all of the retrieved data inside of an object called "response"
     .then(function(response) {
+        $(".photoDump").html("");
+        resp = response;
+        for (i = 0; i < resp.photos.length; i++){
 
-      // Log the resulting object
-      console.log(response);
-        responseVar = response;
-    })
+            if(camChoice == resp.photos[i].camera.name){
+                var imageVar = $("<img>");
+                imageVar.addClass("imageLink");
+                imageVar.attr("src", resp.photos[i].img_src);
+                $(".photoDump").append(imageVar);
+
+
+                console.log(resp.photos[i].img_src);
+            }
+        }
+    })}
+
+$(".cameraBtn").on("click", function (){
+    camChoice = $(this).attr("id");
+    ajaxCall();
+})
+
+// $(document).on("click", function(){
+
+// })
+//#FE8360
